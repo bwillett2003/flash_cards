59cdb1bfc3925cfa97d93563b304def75258de01
Bryan Willett <btwillett2003@gmail.com>
2024-07-07 13:06:11 -0500

    Added card file

---
+++ lib/card.rb
@@ -0,0 +1,25 @@
+class Card
+    attr_reader :card
+                :question
+                :answer
+                :category
+
+    def initialize(question, answer, category)
+        @question = question
+        @answer = answer
+        @category = category
+    end
+
+    def question
+        question = "What is the capital of Alaska?"
+    end
+
+    def answer
+        answer = "Juneau"
+    end
+
+    def category
+        category = :Geography
+    end
+
+end
